f2740727c6a1e06bfa92bcc202b07070a03ca873
Ajay <ajayworking6309@gmail.com>
2026-06-23 15:03:19 +0000

    user routh protection fix

--- backend/src/features/user/user.routes.js
+++ backend/src/features/user/user.routes.js
@@ -1,17 +1,18 @@
 import express from "express"
 import { userRegisteration,userSignIn,getAllUsers,userLogout } from "./user.controller.js"
+import { authMiddleware } from "../../middlewares/auth.js"
 
 
 
 
-
+ 
 
 const userRouter=express.Router()
 
 userRouter.route("/signUp").post(userRegisteration)
 userRouter.route("/signIn").post(userSignIn)
-userRouter.route("/logout").post(userLogout)
-userRouter.route("/allUsers").get(getAllUsers)
+userRouter.route("/logout").post(authMiddleware, userLogout)
+userRouter.route("/allUsers").get(authMiddleware, getAllUsers)
 
 
 

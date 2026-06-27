545407c1c7056b33c591d5b077530c62d2f9ccc7
Ajay <ajayworking6309@gmail.com>
2026-06-27 10:56:29 +0000

    updates

--- frontend/src/Socket.jsx
+++ frontend/src/Socket.jsx
@@ -10,7 +10,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,          // 🔥 IMPORTANT
   transports: ["websocket"],
   withCredentials: true,       // 🔥 REQUIRED for cookies

ddee9db3a8d34beb388e8f34f76410982faa0ab7
Ajay <ajayworking6309@gmail.com>
2026-06-29 10:31:51 +0000

    updates

--- backend/app.js
+++ backend/app.js
@@ -22,13 +22,14 @@ app.use(
 
 // 👇 VERY IMPORTANT (handles preflight requests)
 // app.options("*", cors())
+
+app.use(express.json())
 app.use(cookieParser())
 app.use((req, res, next) => {
   console.log("Incoming Cookie Header:", req.headers.cookie);
   next();
 });
 
-app.use(express.json())
 app.use("/api/messages",messageRouter)
 app.use("/api/user", userRouter)
 app.use("/api/room", roomRouter)

5d8cabfbe0eef9800d482134564642483f72f46e
Ajay <ajayworking6309@gmail.com>
2026-06-30 03:54:41 +0000

    socket auth chnage

--- backend/src/middlewares/socketAuth.js
+++ backend/src/middlewares/socketAuth.js
@@ -1,30 +1,56 @@
 
 
+// import jwt from "jsonwebtoken";
+// import * as cookie from "cookie";
+
+// export const socketAuth = (socket, next) => {
+//   try {
+//     // console.log("Handshake headers:", socket.handshake.headers);
+
+//     const rawCookie = socket.handshake.headers.cookie;
+
+//     if (!rawCookie) {
+//       console.log("❌ No rawCookie");
+//       return next(new Error("Unauthorized"));
+//     }
+    
+//     const cookies = cookie.parse(rawCookie);
+//     const jwtToken = cookies.jwtToken;
+//   //  console.log("jwtToken",jwtToken)
+//     if (!jwtToken) {
+//       console.log("❌ No jwtToken in cookie");
+//       return next(new Error("Unauthorized"));
+//     }
+
+//     const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+
+//     // console.log("✅ Token verified:", decoded);
+
+//     socket.user = {
+//       id: decoded.id,
+//     };
+
+//     next();
+//   } catch (err) {
+//     console.log("🔥 JWT VERIFY ERROR:", err.message);
+//     next(new Error("Invalid or expired token"));
+//   }
+// };
+
+
 import jwt from "jsonwebtoken";
-import * as cookie from "cookie";
 
 export const socketAuth = (socket, next) => {
   try {
-    // console.log("Handshake headers:", socket.handshake.headers);
+    const authHeader = socket.handshake.headers.authorization;
 
-    const rawCookie = socket.handshake.headers.cookie;
-
-    if (!rawCookie) {
-      console.log("❌ No rawCookie");
-      return next(new Error("Unauthorized"));
-    }
-    
-    const cookies = cookie.parse(rawCookie);
-    const jwtToken = cookies.jwtToken;
-  //  console.log("jwtToken",jwtToken)
-    if (!jwtToken) {
-      console.log("❌ No jwtToken in cookie");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return next(new Error("Unauthorized"));
     }
 
-    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+    const token = authHeader.split(" ")[1];
 
-    // console.log("✅ Token verified:", decoded);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     socket.user = {
       id: decoded.id,
@@ -32,7 +58,6 @@ export const socketAuth = (socket, next) => {
 
     next();
   } catch (err) {
-    console.log("🔥 JWT VERIFY ERROR:", err.message);
     next(new Error("Invalid or expired token"));
   }
 };
